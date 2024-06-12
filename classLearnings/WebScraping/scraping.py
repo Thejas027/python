37f32bd1ba61b67b50716ed124e0c6de8fca6959
Thejas <teju270934@gmail.com>
2024-06-12 22:55:47 +0530

    started web scraping section where here an request is made to websites to download any image from the websites

---
+++ classLearnings/WebScraping/scraping.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Get the Wikipedia page content
+
+res = requests.get('https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)')
+soup = BeautifulSoup(res.text, 'lxml')
+
+# Find the second image with the class 'mw-file-element'
+
+computer = soup.select('.mw-file-element')[1]
+image_url = 'https:' + computer['src']
+
+# Download the image
+
+img_data = requests.get(image_url).content
+with open('Deep_Blue.jpg', 'wb') as handler:
+      handler.write(img_data)
+
+print("Image downloaded as 'Deep_Blue.jpg'")
